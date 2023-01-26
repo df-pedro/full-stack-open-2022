1f87d504cc9b41af3c83b76d90c8d538a1549247
Pedro Freires <pdf.publico@gmail.com>
2023-01-26 14:48:29 -0300

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -25,11 +25,33 @@ const FeedbackButtons = ({ setGood, setNeutral, setBad }) => {
 }
 
 const Statistics = ({ good, neutral, bad }) => {
+  
+  const totalRatings = (good, neutral, bad) => {
+    return good + neutral + bad;
+  }
+
+  const averageScore = (good, neutral, bad) => {
+    const WEIGHT_GOOD = 1;
+    const WEIGHT_NEUTRAL = 0;
+    const WEIGHT_BAD = -1;
+    const total = totalRatings(good, neutral, bad);
+    const average = (WEIGHT_GOOD * good + WEIGHT_NEUTRAL * neutral + WEIGHT_BAD * bad)/total;
+    return average || 0;
+  }
+
+  const positiveRatio = (good, neutral, bad) => {
+    const total = totalRatings(good, neutral, bad);
+    return good/total || 0;
+  }
+  
   return (
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {totalRatings(good, neutral, bad)}</p>
+      <p>average {averageScore(good, neutral, bad)}</p>
+      <p>positive {positiveRatio(good, neutral, bad)}</p>
     </div>
   )
 }
